259d3ae97d71bfe2980da6bc53dc07c60fc71038
realic001 <kingsura002@gmail.com>
2025-06-08 13:01:29 +0530

    Update Start.py

--- handlers/Start.py
+++ handlers/Start.py
@@ -3,10 +3,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-@Client.on_message(filters.command("start"))
-async def start_handler(client: Client, message: Message):
-    await message.reply_text(
-        "👋 Hello! I'm zoro auto rename Bot!\n\n"
-        "📁 I can rename files, set thumbnails, format filenames, and much more.\n"
-        "Use the menu or send a file to get started!"
-    )
+def start_handlers(app: Client):
+    @app.on_message(filters.command("start"))
+    async def start_handler(client: Client, message: Message):
+        await message.reply_text(
+            "👋 Hello! I'm Zoro Auto Rename Bot!\n\n"
+            "📁 I can rename files, set thumbnails, format filenames, and much more.\n"
+            "Use the menu or send a file to get started!"
+        )
